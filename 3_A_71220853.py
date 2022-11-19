4bbf036a8435ffeacdc4c34ffc3b51ad0adb9be7
Axlnd3r <118033665+Axlnd3r@users.noreply.github.com>
2022-11-19 08:51:52 +0700

    Add files via upload

---
+++ 3_A_71220853.py
@@ -0,0 +1,7 @@
+a = input ("masukkan panjang: ")
+b = input ("masukkan lebar: ")
+c = " Area tersebut membutuhkan"
+d = " kaleng cat"
+e = input (" Masukkan jari-jari : ")
+
+print (c, round(((int(a) * int (b))+ (1/2 * 22/7 * int (e)**2)) / 15,0),d)
---
+++ 4_A_71220853.py
@@ -0,0 +1,14 @@
+print ("===============CAFE===========")
+print ("================MASUKKAN JUMLAH PESANAN=================")
+
+a = input ("CAPPUCINO             DISKON 50% Rp 25.000 :")
+b = input ("VANILLA LATTE         DISKON 65% Rp 22.000 :")
+c = input ("AMERICANO             DISKON 35% Rp 30.000 :")
+d = input ("BREWED COFFEE         DISKON 40% Rp 20.000 :")
+e = "TOTAL CAPPUCINO     :"
+f = "TOTAL VANILLA LATTE :"
+g = "TOTAL AMERICANO     :"
+h = "TOTAL BREWED COFFEE :"
+
+
+
